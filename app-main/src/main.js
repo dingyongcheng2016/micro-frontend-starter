9f026a18f09229b5e41834fd618a8007c1dfa45d
srd13523063674 <dingyongcheng-it@bestpay.com.cn>
2023-10-08 17:14:23 +0800

    feat: 提交主应用

---
+++ app-main/src/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css'
+
+import Element from 'element-ui'
+
+// 数据字典
+import dict from './components/Dict'
+
+// 权限指令
+import checkPer from '@/utils/permission'
+import permission from './components/Permission'
+import './assets/styles/element-variables.scss'
+
+// global css
+import './assets/styles/index.scss'
+
+import App from './App'
+import store from './store'
+import router from './router/routers'
+
+import './assets/icons' // icon
+import './router/index' // permission control
+
+import formCreate from '@form-create/element-ui'
+
+
+Vue.use(VueCompositionAPI)
+
+
+Vue.use(checkPer)
+Vue.use(permission)
+Vue.use(dict)
+Vue.use(Element, {
+  size: Cookies.get('size') || 'small' // set element-ui default size
+})
+
+Vue.use(formCreate)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
---
+++ app-main/src/router/routers.js
@@ -0,0 +1,82 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Layout from '../layout/index'
+
+Vue.use(Router)
+
+export const constantRouterMap = [
+  { path: '/login',
+    meta: { title: '登录', noCache: true },
+    component: (resolve) => require(['@/views/login'], resolve),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: (resolve) => require(['@/views/features/404'], resolve),
+    hidden: true
+  },
+  {
+    path: '/401',
+    component: (resolve) => require(['@/views/features/401'], resolve),
+    hidden: true
+  },
+  {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path*',
+        component: (resolve) => require(['@/views/features/redirect'], resolve)
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: (resolve) => require(['@/views/home'], resolve),
+        name: 'Dashboard',
+        meta: { title: '首页', icon: 'index', affix: true, noCache: true }
+      }
+    ]
+  },
+  {
+    path: '/user',
+    component: Layout,
+    hidden: true,
+    redirect: 'noredirect',
+    children: [
+      {
+        path: 'center',
+        component: (resolve) => require(['@/views/system/user/center'], resolve),
+        name: '个人中心',
+        meta: { title: '个人中心' }
+      }
+    ]
+  },
+  {
+    path: '/formCreate',
+    component: Layout,
+    hidden: false,
+    redirect: 'noredirect',
+    children: [
+      {
+        path: 'index',
+        component: (resolve) => require(['@/views/formCreate/index'], resolve),
+        name: '表单生成器',
+        meta: { title: '表单生成器' }
+      }
+    ]
+  }
+]
+
+export default new Router({
+  // mode: 'hash',
+  mode: 'history',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
